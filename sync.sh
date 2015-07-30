0a0d8f0528aabf62c22dc830b3093c5674b12775
c0mtrex <gshaunbrowne@gmail.com>
2015-07-30 11:45:09 -0400

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eu
-# See http://wiki.magicleap.ds/display/JIRACONF/Confluence+Profile+Synchronizer+info
+# 
 
 if [[ "$(uname -n)" != ml-confluence-01 ]]
 then
@@ -8,11 +8,11 @@ then
 fi
 
 /usr/local/rvm/wrappers/default/ruby /opt/atlassian/activedirectory-confluence-user-profile-sync/sync_ad_confluence.rb \
-    --ldaphost "magicleap.ds" \
-    --binddn="CN=Service Account for LDAP search and Bind,OU=SERVICE ACCOUNTS,OU=ADMIN,DC=magicleap,DC=ds" \
-    --bindpassword="47g9GXvP" \
-    --basedn="OU=ADMIN,DC=magicleap,dc=ds" \
-    --confbaseurl="http://wiki.magicleap.ds" \
+    --ldaphost "AD server" \
+    --binddn="CN=Service Account for LDAP search and Bind,OU=SERVICE ACCOUNTS,OU=ADMIN,DC=domain,DC=ds" \
+    --bindpassword="password" \
+    --basedn="OU=ADMIN,DC=domain,dc=ds" \
+    --confbaseurl="http://" \
     --confuser="_profilesyncer" \
-    --confpassword="xnIxQcPt4mu0SvORLNo9"  \
+    --confpassword="password"  \
     $*
